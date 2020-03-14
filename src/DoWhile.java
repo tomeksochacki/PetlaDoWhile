ccb02ae4b49271fc84a80647841afe570710f6d9
tomek <tomeksochacki@wp.pl>
2020-03-14 11:21:18 +0100

    Initialize repozitory

---
+++ src/DoWhile.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int zmienna;
+
+        do { System.out.println("Enter to number: ");
+        zmienna = scanner.nextInt();
+        }while (zmienna != 6);
+
+
+
+    }
+
+
+
+}
+
